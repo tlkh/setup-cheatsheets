184deb8349b11fb075873d13ce8baccfc05ae388
Timothy Liu <tlkh@users.noreply.github.com>
2018-01-01 22:21:11 +0800

    updated opencv install script to includ rpi optimisations and opencv release 3.4.0

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Setup Cheatsheets
 Cheatsheets for Various Quick and Painless Setups
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
--- install_opencv.sh
+++ install_opencv.sh
@@ -1,49 +1,51 @@
 #!/bin/bash  
-echo "Installing OpenCV 3.3.1. This will take a while."  
+echo "Installing OpenCV 3.4.0. This will take a while."  
 echo "Installing supporting packages."
 echo "Updating repositories first"  
-sudo apt-get update
+sudo apt update
 echo "Done. Proceeding with install." 
-sudo apt-get install build-essential cmake pkg-config -y
-sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev -y
-sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev -y
-sudo apt-get install libxvidcore-dev libx264-dev -y
-sudo apt-get install libgtk2.0-dev -y
-sudo apt-get install libatlas-base-dev gfortran -y
-sudo apt-get install python2.7-dev python3-dev -y
+sudo apt install build-essential cmake pkg-config -y
+sudo apt install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev -y
+sudo apt install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev -y
+sudo apt install libxvidcore-dev libx264-dev -y
+sudo apt install libgtk2.0-dev libgtk-3-dev -y
+sudo apt install libcanberra-gtk* -y
+sudo apt install libatlas-base-dev gfortran -y
+sudo apt install python2.7-dev python3-dev python-pip python3-pip -y
+sudo apt clean -y
 echo "Done."
 cd ~
-echo "Downloading OpenCV 3.3.1 sources (including opencv_contrib)."
-wget -O opencv.zip https://github.com/Itseez/opencv/archive/3.3.1.zip
-wget -O opencv_contrib.zip https://github.com/Itseez/opencv_contrib/archive/3.3.1.zip
+echo "Downloading OpenCV 3.4.0 sources (including opencv_contrib)."
+wget -O opencv.zip https://github.com/opencv/opencv/archive/3.4.0.zip
+wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/3.4.0.zip
 echo "Extracting archives."
 unzip opencv.zip
 unzip opencv_contrib.zip
-echo "Installing pip"
-wget https://bootstrap.pypa.io/get-pip.py
-sudo python get-pip.py
 echo "Installing numpy"
-pip install numpy
+pip3 install numpy
 echo "Deleting pip cache"
 sudo rm -rf ~/.cache/pip
 echo "Preparing to build OpenCV"
-cd ~/opencv-3.3.1/
+cd ~/opencv-3.4.0/
 mkdir build
 cd build
-echo "Not building examples."
+echo "Not building examples and tests"
+echo "Including contrib modules"
+echo "Including NEON and VFPV3 optimisations (RPi 3)"
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
     -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.4.0/modules \
+    -D ENABLE_NEON=ON \
+    -D ENABLE_VFPV3=ON \
+    -D BUILD_TESTS=OFF \
     -D INSTALL_PYTHON_EXAMPLES=OFF \
-    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.3.1/modules \
     -D BUILD_EXAMPLES=OFF ..
-echo "Now compiling OpenCV."
+echo "Now compiling OpenCV..."
 echo "This will take QUITE A WHILE!!"
-echo "(single core build - recommended for Raspberry Pi.)"
-make # -j4 (quad core build)
+echo "(dual core build - recommended for RPi 2/3)"
+make -j2 # -j4 (quad core build)
 echo "Congratulations, it is done. Installing now."
 sudo make install
 sudo ldconfig
 cd ~
-echo "Removing installation files"
-sudo rm -R opencv-3.3.1 opencv_contrib-3.3.1 opencv-3.3.1.zip opencv_contrib-3.3.1.zip get-pip.py
 echo "Goodbye!"
