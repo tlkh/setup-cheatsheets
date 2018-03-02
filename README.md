2c8bbc5a21736cbbc80e9bdd3e499955fc8c22c0
Timothy Liu <tlkh@users.noreply.github.com>
2018-03-02 22:11:14 +0800

    updated readme and pi zero opencv script

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # Setup Cheatsheets
 Cheatsheets for Various Quick and Painless Setups
 
-For fastai setup (without Anaconda/virtual env):
+## Raspberry Pi
+
+```
+curl https://raw.githubusercontent.com/tlkh/setup-cheatsheets/master/install_opencv.sh | sudo bash
+```
+
+## Google Cloud Platform (GPU)
+
+For fast.ai v2 setup (without Anaconda/virtual env):
 ```
 curl https://raw.githubusercontent.com/tlkh/setup-cheatsheets/master/fastai_noconda.sh | sudo bash
 ```
-CUDA only (GCP)
+CUDA only
 ```
 curl https://raw.githubusercontent.com/tlkh/setup-cheatsheets/master/cuda_gcp.sh | sudo bash
 ```
--- install_opencv_pizero.sh
+++ install_opencv_pizero.sh
@@ -1,5 +1,5 @@
 #!/bin/bash  
-echo "Installing OpenCV 3.4.0. This will take a while."  
+echo "Installing OpenCV 3.4.1. This will take a while."  
 echo "Installing supporting packages."
 echo "Updating repositories first"  
 sudo apt-get update
@@ -14,18 +14,16 @@ sudo apt-get install python-dev python3-dev python-pip python3-pip -y
 sudo apt-get clean -y
 echo "Done."
 cd ~
-echo "Downloading OpenCV 3.4.0 sources (including opencv_contrib)."
-wget -O opencv.zip https://github.com/opencv/opencv/archive/3.4.0.zip
-wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/3.4.0.zip
+echo "Downloading OpenCV 3.4.1 sources (including opencv_contrib)."
+wget -O opencv.zip https://github.com/opencv/opencv/archive/3.4.1.zip
+wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/3.4.1.zip
 echo "Extracting archives."
 unzip opencv.zip
 unzip opencv_contrib.zip
 echo "Installing numpy"
-sudo apt-get install python3-numpy -y
-echo "Deleting pip cache"
-sudo rm -rf ~/.cache/pip
+sudo apt-get install python3-numpy python3-matplotlib -y
 echo "Preparing to build OpenCV"
-cd ~/opencv-3.4.0/
+cd ~/opencv-3.4.1/
 mkdir build
 cd build
 echo "Not building examples and tests"
@@ -34,7 +32,7 @@ echo "Including contrib modules"
 sudo make clean
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
     -D CMAKE_INSTALL_PREFIX=/usr/local \
-    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.4.0/modules \
+    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.4.1/modules \
     -D BUILD_TESTS=OFF \
     -D INSTALL_PYTHON_EXAMPLES=OFF \
     -D BUILD_EXAMPLES=OFF ..
