20b381b5b1e3a452ab3d0094745f4e47998db7af
Timothy Liu <tlkh@users.noreply.github.com>
2018-06-11 01:41:29 +0800

    Updated CUDA and cuDNN versions

--- cuda_gcp.sh
+++ cuda_gcp.sh
@@ -2,7 +2,7 @@
 echo 'Updating repository...'
 sudo apt update
 echo 'Upgrading packages...'
-sudo apt update -y --force-yes && sudo apt dist-update -y --force-yes
+sudo apt update -y && sudo apt dist-update -y
 echo 'Cleaning up...'
 sudo apt autoremove -y && sudo apt clean
 echo 'Done! Moving on with install.'
@@ -13,12 +13,12 @@ sudo apt install python3-dev python3-pip python-dev python-pip -y
 echo 'Now installing Python 3 libraries:'
 sudo pip3 install setuptools pip wheel --upgrade
 sudo pip3 install numpy scipy matplotlib jupyter ipywidgets pillow
-sudo pip3 install bcolz seaborn pandas sklearn keras tensorflow-gpu
+sudo pip3 install bcolz seaborn pandas sklearn keras tensorflow
 sudo pip3 install opencv-contrib-python
+sudo rm -R ~/.jupyter
 echo '... enabling widgets for Jupyter'
 jupyter nbextension enable --py widgetsnbextension --sys-prefix
 echo '... generating config'
-sudo rm -R ~/.jupyter
 jupyter notebook --generate-config
 echo 'Jupyter Notebook will be made accessible from:'
 sudo ifconfig | grep inet
@@ -35,17 +35,18 @@ echo 'All hail the Nvidia gods'
 sudo apt -y install qtdeclarative5-dev qml-module-qtquick-controls
 sudo add-apt-repository ppa:graphics-drivers/ppa -y
 sudo apt update
+mkdir ~/downloads/
 cd ~/downloads/
-sudo rm cuda-repo-ubuntu1604_9.0.176-1_amd64.deb cudnn-9.1-linux-x64-v7.tgz
-echo '(1/3 - Downloading CUDA 9.0)'
-wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+sudo rm *
+echo '(1/3 - Downloading CUDA 9.2)'
+wget https://developer.nvidia.com/compute/cuda/9.2/Prod/local_installers/cuda-repo-ubuntu1604-9-2-local_9.2.88-1_amd64
+sudo dpkg -i cuda-repo-ubuntu1604-9-2-local_9.2.88-1_amd64.deb
 sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
 sudo apt update
 sudo apt install cuda -y
 echo '(2/3 - Downloading cuDNN 7.0)'
-wget http://files.fast.ai/files/cudnn-9.1-linux-x64-v7.tgz
-tar xf cudnn-9.1-linux-x64-v7.tgz
+wget https://tlkh.design/downloads/cudnn-9.2-linux.tgz
+tar xf cudnn-9.2-linux.tgz
 echo '(3/3 - Moving files to proper paths)'
 sudo cp cuda/include/*.* /usr/local/cuda/include/
 sudo cp cuda/lib64/*.* /usr/local/cuda/lib64/
