f81fbfc811d796ca4f3df7a195752ded600e3c92
Timothy Liu <tlkh@live.co.uk>
2019-01-01 16:03:46 +0800

    Update install_docker_nvidia_ubuntu.sh

--- install_docker_nvidia_ubuntu.sh
+++ install_docker_nvidia_ubuntu.sh
@@ -6,7 +6,9 @@ echo "Set non-interactive frontend"
 echo "Script will run without any prompts"
 DEBIAN_FRONTEND=noninteractive
 
+echo "\n###\n"
 echo "Installing prerequisites for Docker CE"
+echo "\n###\n"
 
 apt-get update
 apt-get remove docker docker-engine docker.io -y
@@ -25,16 +27,22 @@ apt-key fingerprint 0EBFCD88
    $(lsb_release -cs) \
    stable"
 
+echo "\n###\n"
 echo "Installing Docker CE"
+echo "\n###\n"
 
 apt-get update
 apt-get install docker-ce -y
 
+echo "\n###\n"
 echo "Testing Docker CE with hello-world container"
+echo "\n###\n"
 
 docker run --rm hello-world
 
+echo "\n###\n"
 echo "Installing nvidia-docker"
+echo "\n###\n"
 
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | apt-key add -
 
@@ -43,9 +51,18 @@ distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
 curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | tee /etc/apt/sources.list.d/nvidia-docker.list
 
 apt-get update
-apt-get install nvidia-docker2
+apt-get install nvidia-docker2 -y
 
 echo "Installed nvidia-docker"
-echo "Restart Docker daemon"
+echo "Restarting Docker daemon"
 
+pkill -SIGHUP dockerd
+
+echo "\n###\n"
+echo "Testing nvidia-docker"
+echo "\n###\n"
+
+docker run --runtime=nvidia --rm nvidia/9.0-runtime-ubuntu16.04 nvidia-smi
+
+echo "\n###\n"
 echo "Finished with no errors."
